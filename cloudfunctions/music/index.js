5456965ca296d9512024528b291043bc9133b163
yjd <674032206@qq.com>
2020-08-05 11:54:56 +0800

    新增 NeteaseCloudMusicApi 请求网易云音乐数据方式

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -3,10 +3,8 @@ const cloud = require('wx-server-sdk');
 // 一个用户只能在一个环境中创建 50 个云函数，如果小程序很复杂的话，云函数就不够用了
 // 可以使用 云函数路由库 —— tcb-router
 const TcbRouter = require('tcb-router');
-const axios = require('axios');
-
-// const BASE_URL = 'http://localhost:4000/';
-const BASE_URL = 'http://musicapi.xiecheng.live';
+// const axios = require('axios');
+const {playlist_detail,song_url,lyric} = require('NeteaseCloudMusicApi');
 
 cloud.init({
     env: cloud.DYNAMIC_CURRENT_ENV
@@ -16,6 +14,8 @@ cloud.init({
 const db = cloud.database();
 // 获取播放列表集合的引用
 const playlistCollection = db.collection('playlist');
+
+// const BASE_URL = 'http://musicapi.xiecheng.live';
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -32,25 +32,42 @@ exports.main = async (event, context) => {
             .get()
     });
 
+    // 这里获取数据有两种调用方式：
+    // 一种是将 NeteaseCloudMusicApi 部署到自己的服务器，然后请求该服务的地址来获取 推荐歌单列表 数据
+    // const URL = 'http://musicapi.xiecheng.live';
+    // const result = await axios.request(URL + '/personalized');
+
+    // 一种是安装 NeteaseCloudMusicApi 包，使用这个包里的 api 去请求数据
+    // https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=%e5%8f%af%e4%bb%a5%e5%9c%a8nodejs%e8%b0%83%e7%94%a8
+
     // 获取歌单下的音乐列表
     app.router('musicList', async (ctx, next) => {
-        const URL = `${BASE_URL}/playlist/detail?id=${parseInt(event.playlistId)}`;
-        const result = await axios.request(URL);
-        ctx.body = result.data;
+        // const URL = `${BASE_URL}/playlist/detail?id=${parseInt(event.playlistId)}`;
+        // const result = await axios.request(URL);
+        const result = await playlist_detail({
+            id: parseInt(event.playlistId)
+        });
+        ctx.body = result.body;
     });
 
     // 根据音乐ID 获取音乐对应的 url
     app.router('musicUrl', async (ctx, next) => {
-        const URL = `${BASE_URL}/song/url?id=${event.musicId}`;
-        const result = await axios.request(URL);
-        ctx.body = result.data;
+        // const URL = `${BASE_URL}/song/url?id=${event.musicId}`;
+        // const result = await axios.request(URL);
+        const result = await song_url({
+            id:event.musicId
+        });
+        ctx.body = result.body;
     });
 
     // 获取音乐的歌词数据
     app.router('lyric', async (ctx, next) => {
-        const URL = `${BASE_URL}/lyric?id=${event.musicId}`;
-        const result = await axios.request(URL);
-        ctx.body = result.data;
+        // const URL = `${BASE_URL}/lyric?id=${event.musicId}`;
+        // const result = await axios.request(URL);
+        const result = await lyric({
+            id:event.musicId
+        });
+        ctx.body = result.body;
     });
 
 
